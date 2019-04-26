4eb6d3d81580f415fb62a537f425b4ad6ccba5eb
lazmar <lazmar@users.noreply.github.com>
2019-04-26 13:57:09 +0300

    Изменена фикстура

--- addressbook-web-tests/src/test/java/hw/addressbook/GroupCreation.java
+++ addressbook-web-tests/src/test/java/hw/addressbook/GroupCreation.java
@@ -13,10 +13,6 @@ public class GroupCreation {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testUntitledTestCase() throws Exception {
     wd.get("http://localhost/addressbook/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -26,6 +22,10 @@ public class GroupCreation {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
