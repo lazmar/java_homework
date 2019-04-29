108dc77e621316de2519c8dcdef94c60bbceddb5
lazmar <lazmar@users.noreply.github.com>
2019-04-29 20:58:23 +0300

    Added HelperBase

--- addressbook-web-tests/src/test/java/hw/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/hw/addressbook/appmanager/ContactHelper.java
@@ -5,93 +5,49 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class ContactHelper {
-    private WebDriver wd;
+public class ContactHelper extends HelperBase{
+
 
     public ContactHelper(WebDriver wd) {
-        this.wd=wd;
+        super(wd);
     }
 
     public void submitCreation() {
-      wd.findElement(By.name("submit")).click();
+      click(By.name("submit"));
     }
 
     public void fillContactForm(ContactData contactData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-      wd.findElement(By.name("middlename")).click();
-      wd.findElement(By.name("middlename")).clear();
-      wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-      wd.findElement(By.name("nickname")).click();
-      wd.findElement(By.name("nickname")).click();
-      wd.findElement(By.name("nickname")).clear();
-      wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
-      wd.findElement(By.name("title")).click();
-      wd.findElement(By.name("title")).clear();
-      wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
-      wd.findElement(By.name("company")).click();
-      wd.findElement(By.name("company")).clear();
-      wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
-      wd.findElement(By.name("address")).click();
-      wd.findElement(By.name("address")).clear();
-      wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-      wd.findElement(By.name("home")).click();
-      wd.findElement(By.name("home")).clear();
-      wd.findElement(By.name("home")).sendKeys("111111");
-      wd.findElement(By.name("mobile")).click();
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-      wd.findElement(By.name("work")).click();
-      wd.findElement(By.name("work")).clear();
-      wd.findElement(By.name("work")).sendKeys(contactData.getWork());
-      wd.findElement(By.name("fax")).click();
-      wd.findElement(By.name("fax")).clear();
-      wd.findElement(By.name("fax")).sendKeys(contactData.getFax());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-      wd.findElement(By.name("email2")).click();
-      wd.findElement(By.name("email2")).clear();
-      wd.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
-      wd.findElement(By.name("email3")).click();
-      wd.findElement(By.name("email2")).click();
-      wd.findElement(By.name("email3")).click();
-      wd.findElement(By.name("email3")).click();
-      wd.findElement(By.name("email3")).clear();
-      wd.findElement(By.name("email3")).sendKeys(contactData.getEmail3());
-      wd.findElement(By.name("homepage")).click();
-      wd.findElement(By.name("homepage")).clear();
-      wd.findElement(By.name("homepage")).sendKeys(contactData.getHomepage());
-      wd.findElement(By.name("bday")).click();
-      new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
-      wd.findElement(By.name("bday")).click();
-      wd.findElement(By.name("bmonth")).click();
-      new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
-      wd.findElement(By.name("bmonth")).click();
-      wd.findElement(By.name("byear")).click();
-      wd.findElement(By.name("byear")).clear();
-      wd.findElement(By.name("byear")).sendKeys(contactData.getByear());
-      wd.findElement(By.name("aday")).click();
-      new Select(wd.findElement(By.name("aday"))).selectByVisibleText(contactData.getAday());
-      wd.findElement(By.name("aday")).click();
-      wd.findElement(By.name("amonth")).click();
-      new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(contactData.getAmonth());
-      wd.findElement(By.name("amonth")).click();
-      wd.findElement(By.name("ayear")).click();
-      wd.findElement(By.name("ayear")).clear();
-      wd.findElement(By.name("ayear")).sendKeys(contactData.getAyear());
-      wd.findElement(By.name("address2")).click();
-      wd.findElement(By.name("address2")).clear();
-      wd.findElement(By.name("address2")).sendKeys(contactData.getAddress2());
-      wd.findElement(By.name("phone2")).click();
-      wd.findElement(By.name("phone2")).clear();
-      wd.findElement(By.name("phone2")).sendKeys(contactData.getPhone2());
-      wd.findElement(By.name("notes")).click();
-      wd.findElement(By.name("notes")).clear();
-      wd.findElement(By.name("notes")).sendKeys(contactData.getNotes());
+        type(By.name("firstname"),contactData.getFirstname());
+        type(By.name("middlename"),contactData.getMiddlename());
+        type(By.name("lastname"),contactData.getLastname());
+        type(By.name("nickname"),contactData.getNickname() );
+        type(By.name("title"),contactData.getTitle());
+        type(By.name("company"),contactData.getCompany());
+        type(By.name("address"),contactData.getAddress());
+
+        type(By.name("mobile"),contactData.getMobile());
+        type(By.name("work"),contactData.getWork());
+        type(By.name("fax"),contactData.getFax());
+        type(By.name("email"),contactData.getEmail());
+        type(By.name("email2"),contactData.getEmail2());
+        type(By.name("email3"),contactData.getEmail3());
+        type(By.name("homepage"),contactData.getHomepage());
+        clickAndSelect(By.name("bday"), contactData.getBday());
+        clickAndSelect(By.name("bmonth"), contactData.getBmonth());
+        type(By.name("byear"), contactData.getByear());
+
+
+        clickAndSelect(By.name("aday"), contactData.getAday());
+        clickAndSelect(By.name("amonth"), contactData.getAmonth());
+
+        type(By.name("ayear"),contactData.getAyear());
+
+
+      type(By.name("address2"),contactData.getAddress2());
+      type(By.name("phone2"),contactData.getPhone2());
+      type(By.name("notes"),contactData.getNotes());
+
+
     }
+
 }
---
+++ addressbook-web-tests/src/test/java/hw/addressbook/appmanager/HelperBase.java
@@ -0,0 +1,28 @@
+package hw.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class HelperBase {
+    protected WebDriver wd;
+
+    public HelperBase(WebDriver wd) {
+        this.wd=wd;
+    }
+
+    protected void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    protected void clickAndSelect(By locator, String valueFromContactData) {
+        wd.findElement(locator).click();
+        new Select(wd.findElement(locator)).selectByVisibleText(valueFromContactData);
+    }
+}
