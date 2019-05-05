41a806848bc512e98aae2cee7bb5584497a87a0a
lazmar <lazmar@users.noreply.github.com>
2019-05-05 19:41:50 +0300

    Добавлена проверка на GroupPage (NavigationHelper)

--- addressbook-web-tests/src/test/java/hw/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/hw/addressbook/appmanager/NavigationHelper.java
@@ -20,9 +20,6 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoNewContact() {
-        if (isElementPresent(By.id("maintable"))){
-            return;
-        }
         click(By.linkText("add new"));
     }
 }
